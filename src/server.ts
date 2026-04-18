d98f8ac47a27f20582662a6270cf369ed5a15f86
jphillipe <joao.phillipe1998@gmail.com>
2026-04-17 23:30:53 -0300

    chore: update dependencies and add ESLint configuration
    - Added ESLint and related packages to devDependencies in package.json
    - Updated server.ts to use consistent double quotes for strings
    - Created .vscode/settings.json for editor configuration to format on save
    - Added ESLint configuration file (eslint.config.ts) with recommended settings

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-const x = "2"
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
