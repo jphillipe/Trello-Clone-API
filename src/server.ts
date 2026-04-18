1adcd8f54216bb8bef625dc0ae39523cd398046e
jphillipe <joao.phillipe1998@gmail.com>
2026-04-17 22:44:07 -0300

    chore: initial commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const x = "2"
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
